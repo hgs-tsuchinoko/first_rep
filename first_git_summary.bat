58daeef0756c5aa1304e919374b8c2c589ce187f
hgs-tsuchinoko <b1613690@planet.kanazawa-it.ac.jp>
2018-07-03 00:34:13 +0900

    Update first_git_summary.bat

--- first_git_summary.bat
+++ first_git_summary.bat
@@ -1,1 +1,23 @@
-rem ええと
+
+
+rem このコマンドでgitを初期化する  
+git init
+
+rem このコマンドでgitのインデックスにファイルを追加する
+git add "ファイル名"
+
+rem このコマンドでコミットを行う この時 -m "コメント" でコミットの際のコメントを付けることができる
+git commit -m "コメント"
+
+rem 2つのコマンドを入力してユーザー情報を設定します
+git config --global user.email "you@example.com"
+git config --global user.name "Your Name"
+
+rem このコマンドでgitにリモートリポジトリ（下の例のリモートリポジトリ先はGitHub）情報を追加する 
+git remote add origin https://github.com/hgs-tsuchinoko/"リポジトリ名".git
+
+rem このコマンドでリモートリポジトリに反映させることができます（ユーザー名とパスワードを要求される） 
+git push -u origin master 
+
+rem このコマンドでリポジトリに対してファイルの追加が記録されているかどうかを確認します
+git status
